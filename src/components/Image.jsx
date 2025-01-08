3233bbfa7eb3499bd886e2528e960fee01dfe1d2
malcomcrab <malcomcrabesq@outlook.com>
2025-01-08 15:17:34 +0000

    Followed tutorial, image component fetches a json placeholder. Loading and error states included in the fetch

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
-const Image = () => {
+const useImageURL = () => {
   const [imageURL, setImageURL] = useState(null);
   const [error, setError] = useState(null);
-
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/photos", { mode: "cors" })
@@ -14,20 +14,26 @@ const Image = () => {
         return response.json();
       })
       .then((response) => setImageURL(response[0].url))
-      .catch((error) => setError(error));
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
   }, []);
-  
-  if (error) return <p>A network error was encountered</p>
+
+  return { imageURL, error, loading };
+};
+
+const Image = () => {
+  const { imageURL, error, loading } = useImageURL();
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>A network error was encountered</p>;
 
   return (
-    imageURL && (
-      <>
-        <h1>An image</h1>
-        <img src={imageURL} alt={"placeholder text"} />
-      </>
-    )
+    <>
+      <h1>An image</h1>
+      <img src={imageURL} alt={"placeholder text"} />
+    </>
   );
-  
 };
+
 
 export default Image;
