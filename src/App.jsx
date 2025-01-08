ad37e0435d8a956bfb65b1a25b52e8d6413b0297
malcomcrab <malcomcrabesq@outlook.com>
2025-01-08 14:56:53 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Image from './components/Image'
+import './App.css'
+
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Image />
+      </div>
+      
+    </>
+  )
+}
+
+export default App
